1e752894d5e52a95664ca0215d323a6a932bffee
manhdeptraii <155596926+manhdeptraii@users.noreply.github.com>
2024-11-21 23:39:34 +0700

    giao diện đăng ký đăng nhập

---
+++ DuAn_DoAnNhanh.Client/ViewComponents/SignUpSignInViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using DuAn_DoAnNhanh.Client.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DuAn_DoAnNhanh.Client.ViewComponents
+{
+    public class SignUpSignInViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var user = new RegisterViewModel(); // Tạo một đối tượng  mới
+            return View(user); // Truyền đối tượng  vào view
+        }
+    }
+}
---
+++ DuAn_DoAnNhanh.Manage/Controllers/StatisticalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DuAn_DoAnNhanh.Manage.Controllers
+{
+    public class StatisticalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
